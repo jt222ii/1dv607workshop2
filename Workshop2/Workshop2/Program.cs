6933a60a8868b32b497bd4808f4ed617a36b4abc
jt222ii <jt222ii@student.lnu.se>
2015-09-23 13:15:42 +0200

    123

--- Workshop2/Workshop2/Program.cs
+++ Workshop2/Workshop2/Program.cs
@@ -10,11 +10,7 @@ namespace Workshop2
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("hej´peniasds");
-=======
-            Console.WriteLine("adsasd");
->>>>>>> Jonas
+            Console.WriteLine("dasads");
         }
     }
 }
