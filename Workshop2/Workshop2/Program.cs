b95167a1295033177eeb6e209d2bffd0618cbd94
jt222ii <jt222ii@student.lnu.se>
2015-09-23 14:18:42 +0200

    fix
    asdads

--- Workshop2/Workshop2/Program.cs
+++ Workshop2/Workshop2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Workshop2.Controller;
 using Workshop2.Model;
 
 namespace Workshop2
@@ -11,7 +12,7 @@ namespace Workshop2
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Console.WriteLine("Byter branch");
             Console.WriteLine("dasads");
             Console.WriteLine("hahahahahaahha");
@@ -21,11 +22,11 @@ namespace Workshop2
             Console.WriteLine(boat.Length+ "innan ändring pål");
             boat.Length = 80085;
             Console.WriteLine(boat.Length + "efter ändring pelle");
-=======
-            Controller.User u = new Controller.User();
+
+            User u = new Controller.User();
 
             u.StartApplication();
->>>>>>> Adam
+
         }
     }
 }
