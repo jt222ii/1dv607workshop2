44a65c0defd35796fd2738908bb02ff64cbfb5a5
kn222gn <kn222gn@student.lnu.se>
2015-09-23 13:22:14 +0200

    test

--- Workshop2/Workshop2/Program.cs
+++ Workshop2/Workshop2/Program.cs
@@ -11,6 +11,7 @@ namespace Workshop2
         static void Main(string[] args)
         {
             Console.WriteLine("dasads");
+            Console.WriteLine("hahahahahaahha");
         }
     }
 }
